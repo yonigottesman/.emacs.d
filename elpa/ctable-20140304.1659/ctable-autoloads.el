3fb2429498ef8ce8027a021e44589dd32bf717a6
Yoni Gottesman <yonigo10@gmail.com>
2016-07-26 17:46:21 +0300

    Deleted unwanted folders

--- elpa/ctable-20140304.1659/ctable-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; ctable-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("ctable-pkg.el" "ctable.el") (22226 1863
-;;;;;;  866214 911000))
-
-;;;***
-
-(provide 'ctable-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; ctable-autoloads.el ends here
--- elpa/web-mode-20160212.538/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20160212.538" "major mode for editing web templates" (quote nil))
